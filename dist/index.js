48da09bb8a1224404568cf52509f5fcf6375c62b
Sushant <sushantjarial@Sushants-MacBook-Air.local>
2025-03-26 00:09:49 +0530

    wrote core ws logic for chat

--- dist/index.js
+++ dist/index.js
@@ -2,11 +2,39 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 8080 });
+let clients = [];
 wss.on("connection", (socket) => {
+    socket.on("close", (s) => {
+        clients = clients.filter((user) => {
+            return user.socket !== socket;
+        });
+    });
     socket.on("message", (message) => {
-        console.log(message.toString());
-        setInterval(() => {
-            socket.send("here is a random number: " + Math.random());
-        }, 1000);
+        try {
+            const data = JSON.parse(message.toString());
+            console.log(data);
+            if (data.type == "join") {
+                const user = {
+                    roomid: data.roomid,
+                    name: data.name,
+                    socket
+                };
+                socket.send("joined");
+                clients.push(user);
+            }
+            if (data.type === "chat") {
+                clients.forEach((user) => {
+                    if (user.roomid === data.roomid && user.name !== data.name) {
+                        user.socket.send(JSON.stringify({
+                            name: data.name,
+                            message: data.message
+                        }));
+                    }
+                });
+            }
+        }
+        catch (e) {
+            socket.send("error" + e);
+        }
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,56 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer ,WebSocket} from "ws";
+
 
 const wss =new WebSocketServer({port:8080});
+type user={
+
+    roomid:string,
+    name:string,
+    socket:WebSocket
+}
+
+ let clients:user[] =  []
+
 
 wss.on("connection",(socket)=>{
-    socket.on("message",(message)=>{
-        console.log(message.toString())
-        setInterval(()=>{
-            socket.send("here is a random number: " +Math.random())
-        },1000 ) 
+    socket.on("close",(s)=>{
+        clients=clients.filter((user)=>{
+            return user.socket!==socket
+        })
     })
+    
+
+
+    socket.on("message",(message)=>{
+        try{
+        const data=JSON.parse(message.toString())
+        console.log(data)
+        if(data.type=="join"){
+            const user ={
+                roomid:data.roomid,
+                name:data.name,
+                socket
+            }
+                socket.send("joined")
+            clients.push(user)
+        }
+
+        if(data.type==="chat"){
+            clients.forEach((user)=>{
+                if(user.roomid===data.roomid && user.name!==data.name){
+                user.socket.send(JSON.stringify({
+                    name:data.name,
+                    message:data.message
+                }))
+                }
+            })
+        }
+    }
+    catch(e){
+    socket.send("error"+e)
+    }
+    
+    }
+
+)
 })
